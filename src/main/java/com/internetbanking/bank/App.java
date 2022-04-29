055d180afc1bf556783b5477f7feab3639a68a9e
Dheeraj <102202629+Dheerajthuniki@users.noreply.github.com>
2022-04-29 14:37:13 +0530

    add files

---
+++ src/main/java/com/internetbanking/bank/App.java
@@ -0,0 +1,13 @@
+package com.internetbanking.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
